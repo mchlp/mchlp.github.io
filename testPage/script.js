48f5ab520ae2dc533f75b98f8b0a1a497722b23b
Michael Pu <michael.pu123@gmail.com>
2017-04-24 21:45:18 -0400

    added weather icon

--- testPage/script.js
+++ testPage/script.js
@@ -26,18 +26,18 @@ function showPosition(loc) {
 function showError(error) {
     switch(error.code) {
         case 0:
-            $("#alert").find("p")[0].innerHTML= "Geolocation is not supported by this browser.";
+            $("#alert").find("p")[0].innerText= "Geolocation is not supported by this browser.";
         case error.PERMISSION_DENIED:
-            $("#alert").find("p")[0].innerHTML = "User denied the request for Geolocation."
+            $("#alert").find("p")[0].innerText = "User denied the request for Geolocation."
             break;
         case error.POSITION_UNAVAILABLE:
-            $("#alert").find("p")[0].innerHTML = "Location information is unavailable."
+            $("#alert").find("p")[0].innerText = "Location information is unavailable."
             break;
         case error.TIMEOUT:
-            $("#alert").find("p")[0].innerHTML = "The request to get user location timed out."
+            $("#alert").find("p")[0].innerText = "The request to get user location timed out."
             break;
         case error.UNKNOWN_ERROR:
-            $("#alert").find("p")[0].innerHTML = "An unknown error occurred."
+            $("#alert").find("p")[0].innerText = "An unknown error occurred."
             break;
     }
     $("#alert")[0].style.display="block";
@@ -67,9 +67,40 @@ function getWeather(latitude, longitude) {
 
 //update weather data 
 function updateWeather(data) {
-    temp = Math.round(data["currently"]["temperature"]);
-    loc = data["timezone"].substring(data["timezone"].indexOf("/")+1);
-
-    $("#weatherLoc")[0].innerHTML = loc;
-    $("#weatherTemp")[0].innerHTML = temp+"&deg;C";
+    var temp = Math.round(data["currently"]["temperature"]);
+    var loc = data["timezone"].substring(data["timezone"].indexOf("/")+1).replace("_", " ");
+    var type = data["currently"]["icon"];
+    
+    $("#weatherTemp")[0].innerText = temp+"°C";
+    $("#weatherLoc")[0].innerText = loc;
+    
+    var image;
+    console.log(type);
+    switch(type) {
+        case "clear-day":
+        case "clear-night":
+            image = "sunny.png";
+            break;
+        case "rain":
+            image = "rain.png";
+            break;
+        case "snow":
+        case "sleet":
+            image = "snow.png";
+            break;
+        case "wind":
+        case "fog":
+        case "cloudy":
+            image = "cloudy.png";
+            break;
+        case "partly-cloudy-day":
+        case "partly-cloudy-night":
+            image = "sunAndClouds.png";
+            break;
+        default:
+            image = "question.png";
+            break;
+    }
+    
+    $("#weatherImage").attr("src", "images/"+image);
 }
