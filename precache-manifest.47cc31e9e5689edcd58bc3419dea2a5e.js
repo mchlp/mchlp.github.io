4cad3acdeeb0c6597393e325a33e552cb96ef10c
Michael Pu <michael.pu123@gmail.com>
2019-07-20 00:28:39 -0400

    Updates

---
+++ precache-manifest.47cc31e9e5689edcd58bc3419dea2a5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbb9a2819d72dce3d0aaa3c79ed6845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50228836d5c7d6141c41",
+    "url": "/static/css/main.124c7b53.chunk.css"
+  },
+  {
+    "revision": "ec17dd9d751765d291bb",
+    "url": "/static/js/2.609907aa.chunk.js"
+  },
+  {
+    "revision": "50228836d5c7d6141c41",
+    "url": "/static/js/main.98091330.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "285070138977345fab5482677d4858cb",
+    "url": "/static/media/devpost.28507013.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "3f60edfd3589f7ad589cd03676982f12",
+    "url": "/static/media/linkedin.3f60edfd.png"
+  },
+  {
+    "revision": "c071e0977e6c5d48055e34e8ef2aa969",
+    "url": "/static/media/resume.c071e097.pdf"
+  }
+]);
